5835f77e957e72e42a17f42b962ea9046bedd8c4
delvedor <tommydelved@gmail.com>
2016-07-29 10:27:50 +0200

    Updated exports

--- volatile.js
+++ volatile.js
@@ -7,69 +7,73 @@ const writeFile = require('fs').writeFile
 const keyVal = tyval.string().min(1).toFunction()
 const noop = err => { if (err) throw err }
 
-function createDB () {
-  let database = {}
-  const db = {}
+function Volatile (options) {
+  this.options = options || {}
+  this.db = {}
+}
 
-  db.get = function (key, callback) {
-    callback = callback || noop
-    if (!keyVal(key)) {
-      return callback({ error: 'Key not valid' }, null)
-    }
-    if (database.hasOwnProperty(key)) {
-      return callback(null, database[key])
-    }
-    return callback({ error: 'Key not found' }, null)
+Volatile.prototype.get = function (key, callback) {
+  callback = callback || noop
+  if (!keyVal(key)) {
+    return callback({ error: 'Key not valid' }, null)
   }
-
-  db.put = function (key, value, callback) {
-    callback = callback || noop
-    if (!keyVal(key)) {
-      return callback({ error: 'Key not valid' })
-    }
-    database[key] = value
-    return callback(null)
+  if (this.db.hasOwnProperty(key)) {
+    return callback(null, this.db[key])
   }
+  return callback({ error: 'Key not found' }, null)
+}
 
-  db.del = function (key, callback) {
-    callback = callback || noop
-    if (!keyVal(key)) {
-      return callback({ error: 'Key not valid' })
-    }
-    database = remove(database, key)
-    return callback(null)
+Volatile.prototype.put = function (key, value, callback) {
+  callback = callback || noop
+  if (!keyVal(key)) {
+    return callback({ error: 'Key not valid' })
   }
+  this.db[key] = value
+  return callback(null)
+}
 
-  db.all = function (callback) {
-    callback = callback || noop
-    return callback(null, databaseIterator)
+Volatile.prototype.del = function (key, callback) {
+  callback = callback || noop
+  if (!keyVal(key)) {
+    return callback({ error: 'Key not valid' })
   }
+  this.db = remove(this.db, key)
+  return callback(null)
+}
 
-  db.size = function () {
-    return Object.keys(database).length
-  }
-
-  db.drop = function (callback) {
-    callback = callback || noop
-    database = {}
-    return callback(null)
-  }
-
-  db.dump = function (name, callback) {
-    callback = callback || noop
-    let json = `${JSON.stringify(database, null, 2)}\n`
-    writeFile(`${__dirname}/${name}.json`, json, (err) => {
-      callback(err)
-    })
-  }
-
+Volatile.prototype.all = function (callback) {
+  callback = callback || noop
+  const database = this.db
   const databaseIterator = function * () {
     for (let prop in database) {
       yield { key: prop, value: database[prop] }
     }
   }
-
-  return db
+  return callback(null, databaseIterator)
 }
 
-module.exports = createDB()
+Volatile.prototype.size = function () {
+  return Object.keys(this.db).length
+}
+
+Volatile.prototype.drop = function (callback) {
+  callback = callback || noop
+  this.db = {}
+  return callback(null)
+}
+
+Volatile.prototype.dump = function (name, callback) {
+  callback = callback || noop
+  let json = `${JSON.stringify(this.db, null, 2)}\n`
+  writeFile(`${__dirname}/${name}.json`, json, (err) => {
+    callback(err)
+  })
+}
+
+function builder (options) {
+  if (!(this instanceof Volatile)) {
+    return new Volatile(options)
+  }
+}
+
+module.exports = builder
